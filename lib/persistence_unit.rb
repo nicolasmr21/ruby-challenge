77a4bd422e5ee843c6a876007cc9085251739d8a
nicolasmr21 <nicolasmartinez502@gmail.com>
2021-01-26 13:51:54 -0500

    purge keys done

--- lib/persistence_unit.rb
+++ lib/persistence_unit.rb
@@ -1,7 +1,10 @@
 class PersistenceUnit
 
   def initialize
-    @storage = {} # "KEY" = ["DATA", "FLAGS", "EXPIRATION TIME", "BYTES", "CAS KEY", "MODIFICATION DATE"]
+    @storage = {
+      '1' => ['xxx', 1, 5, 3, 1, Time.now],
+      '2' => ['xxx', 1, 10, 3, 1, Time.now]
+    } # "KEY" = ["DATA", "FLAGS", "EXPIRATION TIME", "BYTES", "CAS KEY", "MODIFICATION DATE"]
     start_purge_thread
   end
 
@@ -27,8 +30,10 @@ class PersistenceUnit
 
   def start_purge_thread
     Thread.new do
-      sleep(5)
-      purge_keys
+      loop do
+        sleep(5)
+        purge_keys
+      end
     end
   end
 
@@ -38,7 +43,7 @@ class PersistenceUnit
       modification_date = value[5]
       diff = Time.now.to_f - modification_date.to_f
       if diff > exptime
-        puts "delete #{key}"
+        puts "PURGE KEY: #{key}"
         @storage.delete(key)
       end
     end
