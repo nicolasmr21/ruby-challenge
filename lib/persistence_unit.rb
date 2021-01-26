df4ce0b6dff24c7720572abc97fe3522625221fe
nicolasmr21 <nicolasmartinez502@gmail.com>
2021-01-26 13:30:25 -0500

    creating thread to purge keys

--- lib/persistence_unit.rb
+++ lib/persistence_unit.rb
@@ -2,6 +2,7 @@ class PersistenceUnit
 
   def initialize
     @storage = {} # "KEY" = ["DATA", "FLAGS", "EXPIRATION TIME", "BYTES", "CAS KEY", "MODIFICATION DATE"]
+    start_purge_thread
   end
 
   def get(key)
@@ -22,6 +23,19 @@ class PersistenceUnit
       return false
     end
     true
+  end
+
+  def start_purge_thread
+    Thread.new do
+      sleep(5)
+      purge_keys
+    end
+  end
+
+  def purge_keys
+    @storage.each do |key, array|
+      exptime = array[2]
+    end
   end
 
 end
