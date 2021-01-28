046f04ccae799a16b825cca23379505fe7306758
nicolasmr21 <nicolasmartinez502@gmail.com>
2021-01-28 14:42:17 -0500

    comment server.rb done

--- lib/server.rb
+++ lib/server.rb
@@ -45,7 +45,9 @@ class Server
     end
   end
 
-  #
+  # This method allows to obtain requests from clients,
+  # read them, activate the processing method and then
+  # write a response.
   def handle_client(client)
     loop do
       request = client.read
@@ -58,7 +60,8 @@ class Server
     end
   end
 
-  #
+  # This method allows you to validate and process a
+  # request using the services offered by the memcached manager.
   def process(request)
     commands, data = format_request(request)
     action = commands.shift.upcase
@@ -70,7 +73,9 @@ class Server
     end
   end
 
-  #
+  # This method allows you to format a request
+  # in order to separate the command and the data in a
+  # form that the memcached administrator can handle.
   def format_request(request)
     lines = request.split("\n")
     [lines[0].split, lines[1]]
